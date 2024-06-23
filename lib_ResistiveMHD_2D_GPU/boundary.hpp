2e7500e6807173ec95b7d76c780d9f784ec86547
Keita <keita19990517@gmail.com>
2024-06-23 14:49:21 +0900

    20240623

---
+++ lib_ResistiveMHD_2D_GPU/boundary.hpp
@@ -0,0 +1,24 @@
+#include <thrust/device_vector.h>
+#include "const.hpp"
+#include "conservation_parameter_struct.hpp"
+
+
+class Boundary
+{
+private:
+
+public:
+
+    void periodicBoundaryX2nd(
+        thrust::device_vector<ConservationParameter>& U
+    );
+
+    void periodicBoundaryY2nd(
+        thrust::device_vector<ConservationParameter>& U
+    );
+
+private:
+
+};
+
+
